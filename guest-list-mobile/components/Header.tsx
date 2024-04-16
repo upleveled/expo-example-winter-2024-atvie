db5b38d3100cf3891004986e9fa7adfcddd202ac
Lukas Prochazka <prochazka.lu@gmail.com>
2024-04-16 15:02:06 +0200

    Add logo to header

--- guest-list-mobile/components/Header.tsx
+++ guest-list-mobile/components/Header.tsx
@@ -1,6 +1,6 @@
 import Constants from 'expo-constants';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { colors } from '../styles/constants';
 
 const styles = StyleSheet.create({
@@ -11,12 +11,21 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight + 10,
     paddingBottom: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 10,
   },
   label: {
     color: colors.text,
     fontFamily: 'Pacifico_400Regular',
     fontSize: 32,
     textAlign: 'center',
+  },
+  logo: {
+    width: 50,
+    height: 50,
   },
 });
 type Props = {
@@ -27,6 +36,11 @@ export default function Header(props: Props) {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
+        <Image
+          style={styles.logo}
+          source={require('../assets/logo.webp')}
+          alt="logo"
+        />
         <Text style={styles.label}>{props.label}</Text>
       </View>
     </SafeAreaView>
