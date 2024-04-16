0c3c0e5728c6ea1ab543e570b75f6dfca607f4b6
Lukas Prochazka <prochazka.lu@gmail.com>
2024-04-16 15:50:46 +0200

    Revert metroRequire changes

--- guest-list-mobile/metroRequire.d.ts
+++ guest-list-mobile/metroRequire.d.ts
@@ -4,34 +4,39 @@
 
 declare namespace __MetroModuleApi {
   interface RequireContext {
+    /** Return the keys that can be resolved. */
+    keys(): string[];
     (id: string): any;
-    /**
-     * Instructs Metro to generate a virtual module in the dependency graph,
-     * with dependencies on every file in the file map that matches the context params.
-     * This can be used to perform dynamic imports from files on disk at build time.
-     * @note Metro parses for `require.context()` calls in the code while building.
-     * @see https://github.com/facebook/metro/pull/822
-     * @see https://webpack.js.org/guides/dependency-management/#requirecontext
-     */
-    context?(
-      directory: string,
-      useSubdirectories?: boolean,
-      regExp?: RegExp,
-      mode?: 'sync' | 'lazy',
-    ): MetroRequireContext;
+    <T>(id: string): T;
+    /** **Unimplemented:** Return the module identifier for a user request. */
+    resolve(id: string): string;
+    /** **Unimplemented:** Readable identifier for the context module. */
+    id: string;
   }
 
-  /**
-   * Returned from `require.context` provided by Metro.
-   * @see https://github.com/facebook/metro/pull/822
-   * @see https://webpack.js.org/guides/dependency-management/#requirecontext
-   */
-  interface MetroRequireContext {
-    (id: string): any;
+  interface RequireFunction {
     /**
-     * @return an array of all possible requests that the context module can handle.
+     * Returns the exports from a dependency. The call is sync. No request to the server is fired. The compiler ensures that the dependency is available.
      */
-    keys(): string[];
+    (path: string): any;
+    <T>(path: string): T;
+
+    /**
+     * **Experimental:** Import all modules in a given directory. This module dynamically updates when the files in a directory are added or removed.
+     *
+     * **Enabling:** This feature can be enabled by setting the `transformer.unstable_allowRequireContext` property to `true` in your Metro configuration.
+     *
+     * @param path File path pointing to the directory to require.
+     * @param recursive Should search for files recursively. Optional, default `true` when `require.context` is used.
+     * @param filter Filename filter pattern for use in `require.context`. Optional, default `.*` (any file) when `require.context` is used.
+     * @param mode Mode for resolving dynamic dependencies. Defaults to `sync`.
+     */
+    context(
+      path: string,
+      recursive?: boolean,
+      filter?: RegExp,
+      mode?: 'sync' | 'eager' | 'weak' | 'lazy' | 'lazy-once',
+    ): RequireContext;
   }
 }
 
